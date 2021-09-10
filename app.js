34e57a3d70272fa2781ae5230e95db030449e5eb
YashShelar007 <yshelar@asu.edu>
2021-09-09 17:24:14 -0700

    Created variables using queryselector

---
+++ app.js
@@ -0,0 +1,5 @@
+var initialPrice = document.querySelector('#initial-price');
+var stockQuantity = document.querySelector('#stock-quantity');
+var currentPrice = document.querySelector('#current-price');
+var calcButton = document.querySelector('#calculate-btn');
+var outputScreen = document.querySelector('#output-screen');
