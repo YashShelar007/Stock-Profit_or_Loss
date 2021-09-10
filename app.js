e404aa13ca7cdd4e1e090d1a80a8cd318b31ba8d
YashShelar007 <yshelar@asu.edu>
2021-09-09 17:39:13 -0700

    created profit loss function

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ var stockQuantity = document.querySelector('#stock-quantity');
 var currentPrice = document.querySelector('#current-price');
 var calcButton = document.querySelector('#calculate-btn');
 var outputScreen = document.querySelector('#output-screen');
+
+function calculateProfitAndLost(initial, quantity, current) {
+    if(current > initial) {
+        //Profit
+        var profit = (current - initial) * quantity;
+        var profitPercent = (profit/initial) * 100;
+        console.log(`Congratualations!! You are in profit of $${profit} and the profit percent is ${profitPercent}%`);
+    }
+    else if(current < initial) {
+        //Loss
+        var loss = (current - initial) * quantity;
+        var lossPercent = (loss/initial) * 100;
+        console.log(`Unfortunately you are in loss of $${loss} and the loss percent is ${lossPercent}%`);
+    }
+    else {
+        //balanced
+        console.log("No Pain No Gain");
+    }
+}
+
